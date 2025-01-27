70b3126e7134df6ca1e5da7586c1e93048d32706
aswini-ra <185046749+aswini-ra@users.noreply.github.com>
2025-01-27 13:22:23 +0530

    created variable file

---
+++ 02-variables.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# adding variales for this script
+
+PERSON1=Aswini 3 no space before and after equal
+PERSON2=Hari
+
+echo "$PERSON1: Hi $PERSON2!"
+echo "$PERSON2: Hi $PERSON1"
+echo "$PERSON1: How are you doing?"
+echo "$PERSON2: I am doing good thankyou!"
